b23102cad6f2ef28d75f0625ffc43e613f327b0e
lucascalegari <lcalegari17@gmail.com>
2017-11-19 17:56:55 -0800

    presenting modal

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
 
+import { ModalController } from 'ionic-angular';
+
 import { Quote } from "../../data/quote.interface";
 import { QuotesService } from "../../services/quotes";
 
+import { QuotePage } from "../quote/quote";
 
 @Component({
   selector: 'page-favorites',
   templateUrl: 'favorites.html',
 })
+
 export class FavoritesPage {
   quotes: Quote[];
 
-  constructor(private quotesService: QuotesService) { }
+  constructor(
+    private quotesService: QuotesService,
+    private modalCtrl: ModalController
+  ) { }
 
   ionViewWillEnter() {
     this.quotes = this.quotesService.getFavoriteQuotes();
+  }
+
+  onViewQuote(quote: Quote) {
+    const modal = this.modalCtrl.create(QuotePage);
+    modal.present();
   }
 }
